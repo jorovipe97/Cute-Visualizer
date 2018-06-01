31b60f784a161da9e04a23b5f4a789383840d3ac
romualdo97 <jorovipe@gmail.com>
2018-06-01 11:57:09 -0500

    Zoom implemented

---
+++ Assets/PinchToZoom.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchToZoom : MonoBehaviour {
+
+    private Camera cam;
+    public float minZoom = 15f;
+    public float maxZoom = 85f;
+    public float perspectiveZoomSpeed = 0.5f;
+    public float orthoZoomSpeed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Se necesitan 2 dedos para el gesto de zoom
+		if (Input.touchCount == 2)
+        {
+            // Obtine informacion del primer touch
+            Touch touch0 = Input.GetTouch(0);
+            // Obtiene informacion del segundo touch
+            Touch touch1 = Input.GetTouch(1);
+
+            Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+            Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+
+            float prevTouchDeltaMag = (touch0PrevPos - touch1PrevPos).magnitude;
+            float actualTouchDeltaMag = (touch0.position - touch1.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - actualTouchDeltaMag;
+
+            if (cam.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Make sure the orthographic size never drops below min zoom.
+                cam.orthographicSize = Mathf.Max(cam.orthographicSize, minZoom);
+            }
+            else
+            {
+                // Otherwise change the field of view based on the change in distance between the touches.
+                cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, minZoom, maxZoom);
+            }
+        }
+	}
+}
